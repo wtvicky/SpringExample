2e9a94d2fc7eb744514d52712ac78b89dedaa5ea
wtvicky <victoriainocenciogoncalves@gmail.com>
2026-04-13 14:19:36 -0300

    repository

---
+++ controle_frequencia/src/main/java/com/senai/backend/controle_frequencia/repositories/PautaRepositor.java
@@ -0,0 +1,11 @@
+package com.senai.backend.controle_frequencia.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.senai.backend.controle_frequencia.models.Pauta;
+
+@Repository
+public interface PautaRepositor extends JpaRepository <Pauta, Integer> {
+    
+}
